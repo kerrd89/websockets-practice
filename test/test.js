59338ecf93e1587bd321e8e982bf804f75285218
David Kerr <kerrd89@gmail.com>
2016-12-21 11:59:44 -0700

    test suite running

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const assert = require('chai').assert;
+
+  describe('the server exists', function() {
+    context('find server', function() {
+      it('should return true', function() {
+        assert.equal();
+      });
+    });
+  });
