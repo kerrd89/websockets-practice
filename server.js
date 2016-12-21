971d5ea0960dfe1b7b52583ef8f4d4a7ae806306
David Kerr <kerrd89@gmail.com>
2016-12-21 09:27:44 -0700

    initial setup of server

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+const port = process.env.PORT || 3000;
+
+var server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log('Listening on port ' + port + '.');
+});
+
+module.exports = server;
